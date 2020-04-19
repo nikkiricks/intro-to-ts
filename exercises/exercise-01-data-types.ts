dc0f81c6a994ee26ef5d0839ec0b4e4916d019b0
nikkiricks <nikki.ricks@gmail.com>
2020-04-19 14:32:26 +1000

    first commit

--- exercises/exercise-01-data-types.ts
+++ exercises/exercise-01-data-types.ts
@@ -3,27 +3,38 @@
  */
 
 const firstName = "John";
+// string
 const lastName = "Doe";
+// string
 const age = 18;
-const gender = 'male';
+// number
+const gender = "male";
+// string
 const birthday = new Date(1991, 11, 25);
+// date
 const yearsOfExperience = "5";
-const profession = 'Web Developer'
-const languages = ['JavaScript', 'NodeJS', 'TypeScript'];
+// string
+const profession = "Web Developer";
+// string
+const languages = ["JavaScript", "NodeJS", "TypeScript"];
+// array
 const frameworks = Array("React", "Vue", "Angular");
+// array
 const address: Address = {
-    street: "123 George St",
-    suburb: 'Sydney',
-    "postcode": 2000,
-    state: "NSW",
-    country: 'Australia'
-}
-const isEmployee = true
-
+  street: "123 George St",
+  suburb: "Sydney",
+  postcode: 2000,
+  state: "NSW",
+  country: "Australia",
+};
+// object
+const isEmployee = true;
+// bollean
 interface Address {
-    street: string;
-    suburb: string;
-    postcode: number;
-    state: string;
-    country: string;
+  street: string;
+  suburb: string;
+  postcode: number;
+  state: string;
+  country: string;
 }
+// object
